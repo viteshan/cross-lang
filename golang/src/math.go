84e113bf39f9a5b7c4520f57f37e0ac60d5ed306
viteshan <viteshan@163.com>
2019-01-22 07:05:21 +0800

    update math lib

--- golang/src/math.go
+++ golang/src/math.go
@@ -1,7 +1,22 @@
-// math.go
 package main
 
+/*
+#include <stdlib.h>
+
+typedef struct {
+	const char* error;
+	const int code;
+} _GoError_;
+
+typedef _GoError_ GoError;
+
+*/
 import "C"
+import (
+	"fmt"
+	"sort"
+	"unsafe"
+)
 
 //export Multiply
 func Multiply(x int64, y int64) int64 {
@@ -9,9 +24,61 @@ func Multiply(x int64, y int64) int64 {
 }
 
 //export Hello
-func Hello(s *C.char) *C.char {
-	return C.CString("hello " + C.GoString(s) + "!")
+func Hello(s string) *C.char {
+	//ss := string(C.GoString(s))
+	aaa := getString()
+	ddd := "hello " + aaa + "!\x00"
+
+	return C.CString(ddd)
+}
+
+//export FreeCs
+func FreeCs(cs *C.char) {
+	C.free(unsafe.Pointer(cs))
+}
+
+func getString() string {
+	return "----"
+}
+
+//export PrintCs
+func PrintCs(cs *C.char) {
+	fmt.Println(C.GoString(cs))
+}
+
+//export Hello2
+func Hello2(s string) *C.char {
+	ddd := "Hello Jie, 杰"
+	return C.CString(ddd)
+	//if e != nil {
+	//	panic(e)
+	//}
+	//return result
+}
+
+//export Sort
+func Sort(vals []int) {
+	sort.Ints(vals)
+}
+
+//export Error
+func Error() C.GoError {
+	return C.GoError{
+		error: C.CString("error---------"),
+		code:  234,
+	}
+}
+
+//export Test
+func Test() int64 {
+	return 0
+}
+
+//export Bol
+func Bol(i bool) {
+	fmt.Println(i)
 }
 
 // main function is required, don't know why!
-func main() {} // a dummy function
+func main() {
+} // a dummy function
